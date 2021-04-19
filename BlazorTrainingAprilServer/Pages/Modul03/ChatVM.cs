6c9ec178d265ece16b974df9cf8968c88aa11076
HannesPreishuber <hannesp@ppedv.de>
2021-04-19 15:26:44 +0200

    Chat Viewmodel

---
+++ BlazorTrainingAprilServer/Pages/Modul03/ChatVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTrainingAprilServer.Pages.Modul03
+{
+    public class ChatVM
+    {
+        public List<string> ChatListe { get; set; } = new List<string>();
+        public Action OnAdd;
+        void Add(string msg)
+        {
+            ChatListe.Add(msg);
+            OnAdd?.Invoke();
+
+        }
+
+    }
+}
