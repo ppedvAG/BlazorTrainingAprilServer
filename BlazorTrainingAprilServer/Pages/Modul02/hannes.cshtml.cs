1f3fdc7707ac7256eff83c3a789bc9ffcec5f7cb
HannesPreishuber <hannesp@ppedv.de>
2021-04-19 11:26:38 +0200

    Mix aus Blazor und Razor

---
+++ BlazorTrainingAprilServer/Pages/Modul02/hannes.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingAprilServer.Pages.Modul02
+{
+    public class hannesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
