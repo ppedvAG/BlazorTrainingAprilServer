b461aed266a54167fcdb5925062a29d71ab9a343
HannesPreishuber <hannesp@ppedv.de>
2021-04-19 14:40:12 +0200

    HttpContrxt für angemeldeten User -ohne kompl- Authentifzierung

---
+++ BlazorTrainingAprilServer/Pages/Modul03/UserVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTrainingAprilServer.Pages.Modul03
+{
+    public class UserVM
+    {
+    }
+}
--- BlazorTrainingAprilServer/Startup.cs
+++ BlazorTrainingAprilServer/Startup.cs
@@ -30,6 +30,8 @@ namespace BlazorTrainingAprilServer
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddHttpContextAccessor();
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
